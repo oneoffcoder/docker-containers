980f842f7bf1b36d741c7210e104f224c8433211
Jee Vang <jvang@bechtel.com>
2019-07-18 20:17:39 -0400

    add more libs; download all spacy en models; download all nltk stuff

--- conda-nlp/setup.sh
+++ conda-nlp/setup.sh
@@ -9,8 +9,12 @@ conda env create -f /tmp/environment.yml
 echo "source activate $NAME" >> ~/.bashrc
 echo "PATH=$ENV_PATH/bin:$PATH" >> ~/.bashrc
 
-$ENV_PATH/bin/python -m ipykernel install --user --name $NAME --display-name $NAME
 $ENV_PATH/bin/python -m spacy download en_core_web_sm
+$ENV_PATH/bin/python -m spacy download en_core_web_md
+$ENV_PATH/bin/python -m spacy download en_core_web_lg
+$ENV_PATH/bin/python -m spacy download en_vectors_web_lg
+$ENV_PATH/bin/python -m nltk.downloader all
+$ENV_PATH/bin/python -m ipykernel install --user --name $NAME --display-name $NAME
 $ENV_PATH/bin/jupyter contrib nbextension install --user
 
 exit 0
