712e9a1749fa6f3940c04234d1bfb2a37e6b86e1
Jee Vang, Ph.D <vangjee@gmail.com>
2019-07-23 17:35:35 -0400

    add some more libs; simply update env; easy demo

--- rpi-scikit/setup.sh
+++ rpi-scikit/setup.sh
@@ -2,18 +2,6 @@
 
 BASE_DIR=/root/miniconda/bin
 CONDA=$BASE_DIR/conda
-PIP=$BASE_DIR/pip
-JUPYTER=$BASE_DIR/jupyter
-PYTHON=$BASE_DIR/python
-
-echo "update conda"
-$CONDA update -n root conda -y
-
-echo "update conda all"
-$CONDA update --all -y
-
-echo "update pip"
-$PIP install --upgrade pip
 
 echo "update environment"
-$CONDA env update -f /tmp/environment.yml --prune
+$CONDA env update -f /tmp/environment.yml
