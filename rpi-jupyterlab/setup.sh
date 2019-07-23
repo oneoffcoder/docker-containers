5d09b3ad386cd8ce5681393ef401dcd78b26de96
Jee Vang, Ph.D <vangjee@gmail.com>
2019-07-23 15:38:50 -0400

    add logging

--- rpi-jupyterlab/setup.sh
+++ rpi-jupyterlab/setup.sh
@@ -6,9 +6,20 @@ PIP=$BASE_DIR/pip
 JUPYTER=$BASE_DIR/jupyter
 PYTHON=$BASE_DIR/python
 
+echo "update conda"
 $CONDA update -n root conda -y
+
+echo "update all for conda"
 $CONDA update --all -y
+
+echo "update pip"
 $PIP install --upgrade pip
+
+echo "install requirements.txt"
 $CONDA install --file /tmp/requirements.txt -y
+
+echo "create new environment"
 $CONDA env create -f /tmp/environment.yml --quiet
+
+echo "install kernel for new environment"
 $PYTHON -m ipykernel install --user --name py36 --display-name "py36"
