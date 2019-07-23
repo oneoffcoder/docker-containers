c34ae3da563701394e5f05c8d6a0c31877968fd0
Jee Vang, Ph.D <vangjee@gmail.com>
2019-07-23 16:19:41 -0400

    set default env to p36; add logging during setup

--- rpi-scikit/setup.sh
+++ rpi-scikit/setup.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-BASE_DIR=/usr/local/miniconda/bin
+BASE_DIR=/root/miniconda/bin
 CONDA=$BASE_DIR/conda
 PIP=$BASE_DIR/pip
 JUPYTER=$BASE_DIR/jupyter
 PYTHON=$BASE_DIR/python
 
+echo "update conda"
 $CONDA update -n root conda -y
+
+echo "update conda all"
 $CONDA update --all -y
+
+echo "update pip"
 $PIP install --upgrade pip
-$CONDA env create -f /tmp/environment.yml --quiet
-$PYTHON -m ipykernel install --name sklearn --display-name "sklearn"
+
+echo "update environment"
+$CONDA env update -f /tmp/environment.yml --prune
