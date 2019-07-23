3a4d5aaa0b09befaf77c2dfa1324b5ec5583e971
Jee Vang, Ph.D <vangjee@gmail.com>
2019-07-22 23:49:01 -0400

    derive image from miniconda image

---
+++ rpi-jupyterlab/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BASE_DIR=/usr/local/miniconda/bin
+CONDA=$BASE_DIR/conda
+PIP=$BASE_DIR/pip
+JUPYTER=$BASE_DIR/jupyter
+PYTHON=$BASE_DIR/python
+
+$CONDA update -n root conda -y
+$CONDA update --all -y
+$PIP install --upgrade pip
+$CONDA install --file /tmp/requirements.txt -y
+$CONDA env create -f /tmp/environment.yml --quiet
+$PYTHON -m ipykernel install --user --name py36 --display-name "py36"
