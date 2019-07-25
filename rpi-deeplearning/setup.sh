fab1927ff6df71520a4c836379f4493f94ec4636
Jee Vang, Ph.D <vangjee@gmail.com>
2019-07-24 23:21:38 -0400

    install additional envs?

--- rpi-deeplearning/setup.sh
+++ rpi-deeplearning/setup.sh
@@ -2,6 +2,13 @@
 
 BASE_DIR=/root/miniconda/bin
 CONDA=$BASE_DIR/conda
+PYTHON=$BASE_DIR/python
 
 echo "update environment"
 $CONDA env update -f /tmp/environment.yml
+
+$CONDA create -n p34 python=3.4 -y
+$CONDA create -n p35 python=3.5 -y
+
+$PYTHON -m ipykernel install --user --name py34 --display-name "py34"
+$PYTHON -m ipykernel install --user --name py35 --display-name "py35"
