022e28c0ac026c094e114cc2a2a1765ed45b50fe
Jee Vang, Ph.D <vangjee@gmail.com>
2019-07-24 20:41:04 -0400

    fix

--- rpi-deeplearning/setup-tensorflow.sh
+++ rpi-deeplearning/setup-tensorflow.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
-wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-armv7l.sh -O /tmp/miniconda.sh
+# https://www.tensorflow.org/install/source_rpi
 
-/bin/bash /tmp/miniconda.sh -b -p /home/pi/miniconda
+git clone https://github.com/tensorflow/tensorflow.git
 
-echo "PATH=/home/pi/miniconda/bin:${PATH}" >> /home/pi/.bashrc
-
-conda update -n root conda -y
-conda update --all -y
-pip install --upgrade pip
-conda config --add channels rpi
-conda install python=3.6 -y
-
-git clone https://github.com/tensorflow/tensorflow.git /home/pi/git/tensorflow
-
-cd /home/pi/git/tensorflow
+cd tensorflow
 
 git checkout r1.14
 
-CI_DOCKER_EXTRA_PARAMS="-e CI_BUILD_PYTHON=python3 -e CROSSTOOL_PYTHON_INCLUDE_PATH=/home/pi/miniconda/include/python3.6m/" \
+CI_DOCKER_EXTRA_PARAMS="-e CI_BUILD_PYTHON=python3 -e CROSSTOOL_PYTHON_INCLUDE_PATH=/usr/include/python3.4" \
     tensorflow/tools/ci_build/ci_build.sh PI-PYTHON3 \
     tensorflow/tools/ci_build/pi/build_raspberry_pi.sh
