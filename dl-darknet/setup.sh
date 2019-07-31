31a576c00cbea515c62cb525607e67de6083f363
Jee Vang, Ph.D <vangjee@gmail.com>
2019-07-31 13:58:39 -0400

    install original darknet too

--- dl-darknet/setup.sh
+++ dl-darknet/setup.sh
@@ -21,7 +21,7 @@ installAnaconda() {
 } 
 
 installDarknet() {
-    echo "installing darknet"
+    echo "installing darknet from alexeyab"
     DARKNET_HOME=/darknet
     # git clone https://github.com/pjreddie/darknet.git $DARKNET_HOME
     git clone https://github.com/AlexeyAB/darknet.git $DARKNET_HOME
@@ -40,6 +40,28 @@ installDarknet() {
     echo "export PATH=\${DARKNET_HOME}:\${PATH}" >> /root/.bashrc
 }
 
+installDn() {
+    echo "installing darknet from pjreddie"
+    DARKNET_HOME=/dn
+    git clone https://github.com/pjreddie/darknet.git $DARKNET_HOME
+    sed -i 's/GPU=0/GPU=1/g' $DARKNET_HOME/Makefile
+    sed -i 's/OPENCV=0/OPENCV=1/g' $DARKNET_HOME/Makefile
+    sed -i 's/CUDNN=0/CUDNN=1/g' $DARKNET_HOME/Makefile
+    cd $DARKNET_HOME
+    make -j 4
+    rm -fr $DARKNET_HOME/data
+    rm -fr $DARKNET_HOME/cfg
+    rm -fr $DARKNET_HOME/backup
+    ln -s /darknet/data $DARKNET_HOME/data
+    ln -s /darknet/cfg $DARKNET_HOME/cfg
+    ln -s /darknet/backup $DARKNET_HOME/backup
+    ln -s /darknet/weight $DARKNET_HOME/weight
+    ln -s /darknet/video $DARKNET_HOME/video
+    ln -s /darknet/image $DARKNET_HOME/image
+    ln -s /darknet/log $DARKNET_HOME/log
+}
+
 updateAptPackages
 installAnaconda
 installDarknet
+installDn
