a1b66342ce22c6f92c57f47969d9c4cd3f17d57f
Jee Vang, Ph.D <vangjee@gmail.com>
2019-07-31 15:33:58 -0400

    update

--- dl-darknet/scripts/annotate.py
+++ dl-darknet/scripts/annotate.py
@@ -1,5 +1,66 @@
-def do_it():
-    print('hello, world!')
+import argparse
+import sys
+import os
+from os import path
+import json
+from PIL import Image, ImageFont, ImageDraw, ImageEnhance
+
+def parse_args(args):
+    """
+    Parses arguments.
+    :return: Arguments.
+    """
+    parser = argparse.ArgumentParser('Makes bounding boxes on images')
+    parser.add_argument('-j', '--json', help='JSON file results from darknet', required=True)
+    parser.add_argument('-d', '--dir', help='output directory for images with bounding box', required=True)
+    return parser.parse_args(args)
+
+
+def annotate(item, o_dir):
+    i_path = item['filename']
+    o_path = os.path.join(o_dir, os.path.basename(i_path)).replace('.jpg', '.png')
+    print('{} to {}'.format(i_path, o_path))
+
+    i_img = Image.open(i_path).convert('RGBA')
+    o_img = Image.new('RGBA', i_img.size)
+
+    o_img.paste(i_img, (0, 0))
+
+    draw = ImageDraw.Draw(o_img, 'RGBA')
+
+    for obj in item['objects']:
+        name = obj['name']
+        x = obj['relative_coordinates']['center_x'] * i_img.size[0]
+        y = obj['relative_coordinates']['center_y'] * i_img.size[1]
+        w = obj['relative_coordinates']['width'] * i_img.size[0]
+        h = obj['relative_coordinates']['height'] * i_img.size[1]
+
+        draw.rectangle(((x, y), (w, h)), fill=None)
+
+    o_img.save(o_path, 'PNG')
+
+
+def do_it(args):
+    json_path = args.json
+    dir_path = args.dir
+    print('attempting to create bounding box from {} to {}'.format(json_path, dir_path))
+
+    if path.exists(json_path) is False:
+        print('{} file does not exists!'.format(json_path))
+        return -1
+    
+    if path.exists(dir_path) is False:
+        print('{} directory does not exists, creating'.format(dir_path))
+        os.makedirs(dir_path)
+
+    with open(json_path, 'r') as json_file:
+        data = json.load(json_file)
+        for item in data:
+            annotate(item, dir_path)
+            break
+        
+    
 
 if __name__ == "__main__":
-    do_it()
+    args = parse_args(sys.argv[1:])
+    do_it(args)
