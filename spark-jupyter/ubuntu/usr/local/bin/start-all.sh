c4b2113288d38983dd4fea19ca5ad50d5f77d3a0
Jee Vang, Ph.D <vangjee@gmail.com>
2019-10-18 22:47:32 -0400

    move files; install graphframe properly; https://github.com/graphframes/graphframes/issues/172

--- spark-jupyter/ubuntu/usr/local/bin/start-all.sh
+++ spark-jupyter/ubuntu/usr/local/bin/start-all.sh
@@ -13,7 +13,9 @@ $SPARK_HOME/sbin/start-history-server.sh
 echo "started spark history"
 
 # pyspark --master spark://localhost:7077 > /tmp/jupyter.log 2>&1 &
-$SPARK_HOME/bin/pyspark --master $PYSPARK_MASTER > /tmp/jupyter.log 2>&1 &
+$SPARK_HOME/bin/pyspark \
+    --packages graphframes:graphframes:0.7.0-spark2.4-s_2.11 \
+    --master $PYSPARK_MASTER > /tmp/jupyter.log 2>&1 &
 echo "started pyspark"
 
 echo "done!"
