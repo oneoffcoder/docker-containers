8970f949f9c51d132a95125b1d0cbaa074897e61
Jee Vang, Ph.D <vangjee@gmail.com>
2020-06-04 17:38:29 -0400

    just tag latest

--- spark-jupyter/tag.sh
+++ spark-jupyter/tag.sh
@@ -2,10 +2,6 @@
 
 ORGANIZATION=oneoffcoder
 REPOSITORY=spark-jupyter
-VERSION=0.0.8
 IMAGEID=spark-jupyter:local
 
-echo ${IMAGEID}
-
-docker tag ${IMAGEID} ${ORGANIZATION}/${REPOSITORY}:${VERSION}
 docker tag ${IMAGEID} ${ORGANIZATION}/${REPOSITORY}:latest
