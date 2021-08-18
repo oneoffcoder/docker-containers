fcba16d6a551c456359a18fe00646b84ef813c7f
Jee Vang, Ph.D <vangjee@gmail.com>
2021-08-18 00:37:42 -0400

    fix bug related to bintray shutting down

--- spark-jupyter/ubuntu/usr/local/bin/start-all.sh
+++ spark-jupyter/ubuntu/usr/local/bin/start-all.sh
@@ -15,7 +15,8 @@ echo "started spark history"
 # pyspark --master spark://localhost:7077 > /tmp/jupyter.log 2>&1 &
 # options: https://gerardnico.com/db/spark/pyspark/pyspark
 $SPARK_HOME/bin/pyspark \
-    --packages graphframes:graphframes:0.7.0-spark2.4-s_2.11 \
+    --packages graphframes:graphframes:0.8.1-spark2.4-s_2.11 \
+    --repositories https://repos.spark-packages.org \
     --master $PYSPARK_MASTER > /tmp/jupyter.log 2>&1 &
 echo "started pyspark"
 
